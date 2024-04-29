7e849662aa1182a6010b87c12747999ea4c3cc35
Mark Karpov <markkarpov92@gmail.com>
2024-04-29 13:21:01 +0200

    Add a directive about sounds

--- Perception/Directive.hs
+++ Perception/Directive.hs
@@ -21,6 +21,7 @@ import Prelude hiding (all)
 data Directive
   = Breath
   | Sky
+  | Sounds
   deriving (Enum, Bounded, Eq, Show)
 
 all :: [Directive]
@@ -30,23 +31,31 @@ name :: Directive -> Text
 name = \case
   Breath -> "breath"
   Sky -> "sky"
+  Sounds -> "sounds"
 
 mnemonic :: Directive -> Char
 mnemonic = \case
   Breath -> 'b'
   Sky -> 's'
+  Sounds -> 'o'
 
 text :: Directive -> Text
 text = \case
   Breath -> "Take a slow deep breath, pay attention to qualities of the air."
-  Sky -> "Concentrate on the sky. How does its color vary? How big does it feel? Are\n\
-\there any clouds? If yes, how many different kinds? What is their\n\
-\character and speed? Stare long enough to see how clouds actually move."
+  Sky ->
+    "Concentrate on the sky. How does its color vary? How big does it feel? Are\n\
+    \there any clouds? If yes, how many different kinds? What is their\n\
+    \character and speed? Stare long enough to see how clouds actually move."
+  Sounds ->
+    "Concentrate on what you hear. How many distinct sound sources are there?\n\
+    \Pay attention to the spacial position and character of sounds. Separate\n\
+    \them mentally, keep track of new sound sources that appear."
 
 precondition :: Directive -> Natural -> State -> Bool
 precondition directive _n st = case directive of
   Breath -> True
   Sky -> stEnvironment st == Outdoors
+  Sounds -> True
 
 effect :: Directive -> Natural -> State -> State
 effect _directive _n st = st {stStamina = stStamina st - 1}
