ec5eda40e3ddf52cfc0617eb0fb166a9fb35a402
Mark Karpov <markkarpov92@gmail.com>
2024-04-30 11:05:48 +0200

    Impose a simplistic vowel/consonant alteration

--- Perception/Routine/Mnemonic.hs
+++ Perception/Routine/Mnemonic.hs
@@ -5,6 +5,12 @@ where
 
 import Numeric.Natural
 
+data Class = Vowel | Consonant
+  deriving (Eq, Show)
+
+classify :: Char -> Class
+classify x = if x `elem` "aeiouy" then Vowel else Consonant
+
 applyPhoneticBias ::
   (Functor f) =>
   (a -> Char) ->
@@ -12,4 +18,9 @@ applyPhoneticBias ::
   f (Natural, a) ->
   f (Natural, a)
 applyPhoneticBias _ Nothing xs = xs
-applyPhoneticBias _ (Just _prevChar) xs = xs
+applyPhoneticBias mnemonic (Just p) xs = g <$> xs
+  where
+    g (n, a) =
+      if classify (mnemonic a) == classify (mnemonic p)
+        then (n, a)
+        else (n * 2, a)
